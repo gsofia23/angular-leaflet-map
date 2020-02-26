bb0e1eeb6bf4795d5a7b90cc4e5a839699ee9aae
Sofia Gonzalez <sofia.gonzalez@openwaterfoundation.org>
2020-02-26 15:56:05 -0700

    Angular Leaflet Map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import 'leaflet';
+declare let L;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-leaflet4';
+export class AppComponent implements OnInit{
+
+  constructor(){
+
+  }
+
+  ngOnInit(){
+    const map = L.map('mapID').setView([ 51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		}).addTo(map);
+  }
+ 
 }
